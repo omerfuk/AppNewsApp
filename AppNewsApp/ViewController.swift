591162fc8926cbdfa9c305917b8109e934cf8ab1
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-06 23:59:05 +0300

    Initial Commit

---
+++ AppNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppNewsApp
+//
+//  Created by Ömer Faruk Kılıçaslan on 6.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
